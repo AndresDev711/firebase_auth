bf7f6b69ac919fa6b314ddc017fa6fb12dddd41f
andreDev711 <andres7condori@gmail.com>
2023-07-02 14:03:52 -0500

    update version 8 to 17 java

--- app/src/main/java/com/andre/dev/demo/authfirebase/ui/views/MainActivity.kt
+++ app/src/main/java/com/andre/dev/demo/authfirebase/ui/views/MainActivity.kt
@@ -2,13 +2,35 @@ package com.andre.dev.demo.authfirebase.ui.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.andre.dev.demo.authfirebase.R
+import android.util.Log
+import com.andre.dev.demo.authfirebase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+  private lateinit var binding: ActivityMainBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+    initListeners()
+  }
+
+  private fun initListeners() {
+    with(binding) {
+      btnSendCode.setOnClickListener {
+        val phone = etPhone.text.toString().trim()
+        if (validatePhone(phone)) {
+          //firebase
+        } else {
+          Log.e("CHECK_PHONE", "PHONE EMPTY")
+        }
+      }
+    }
+  }
+
+  private fun validatePhone(phone: String): Boolean {
+    return phone.isNotEmpty()
   }
 
 
