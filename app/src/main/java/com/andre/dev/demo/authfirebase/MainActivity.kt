fbc31ec55aadf55c8b796f1644fa62f6ce9a668a
andreDev711 <andres7condori@gmail.com>
2023-07-02 12:11:11 -0500

    init project

---
+++ app/src/main/java/com/andre/dev/demo/authfirebase/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.andre.dev.demo.authfirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
